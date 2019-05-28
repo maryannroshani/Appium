05dfb97206b5b15c39105813853bd9a11fecfa78
Maryann Roshani <maryann.roshani@gmail.com>
2019-05-28 21:05:27 +1200

    initial commit

---
+++ SimpleAppium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleAppium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleAppium")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35026095-099c-4f62-88bd-cf127cefb6b4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleAppium/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+
+namespace SimpleAppium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Initiating Appium driver 
+        AppiumDriver<IWebElement> driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability("deviceName", "donatello");
+            capabilities.SetCapability("apppackage", "com.example.calculator");
+
+            //Launch the Android Driver
+            driver = new AndroidDriver<IWebElement>();
+        }
+    }
+}
